5efd07972d9aa12bf2386004ceab27a462261c69
denka <denka@BLENDERTOP>
2026-03-19 14:07:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Reservation_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
