1e77a159ca6a51d31c150c0d4d5e5f3842c1528b
Blender4eto <denka050908@gmail.com>
2026-03-22 12:50:56 +0200

    Added RoomType and ServiceType enums

---
+++ Domain/Enums/ReservationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Reservation_System.Domain.Enums
+{
+    public enum ServiceType
+    {
+        Massage,
+        Spa,
+        Buffet,
+        Babysitting,
+        RoomCleaning,
+        Companion,
+        Laundry,
+        AirportTransfer,
+        Fitness,
+        PoolAccess,
+        Breakfast
+    }
+}
