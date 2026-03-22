9fa43d2c871a9b83d9f6ccb98eca595d923aa208
Blender4eto <denka050908@gmail.com>
2026-03-22 13:09:44 +0200

    Added ReservationService class

---
+++ Domain/Entities/ReservationService.cs
@@ -0,0 +1,16 @@
+﻿using Hotel_Reservation_System.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Reservation_System.Domain.Entities
+{
+    internal class ReservationService
+    {
+        public ServiceType Type { get; set; }
+        public int DurationInDays { get; set; }
+        public decimal Price { get; set; }
+    }
+}
