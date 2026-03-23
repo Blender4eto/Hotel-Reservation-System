f9cf51c90bad6e61c04844333ff406c5bab5b27d
Presi <peciceci736@gmail.com>
2026-03-23 08:59:14 -0900

    Add IGuestRepository interface for guest management
    Introduced the IGuestRepository interface in the Application namespace to define methods for retrieving all guests, fetching a guest by ID, and adding new guests. Added necessary using directives for collections and the Guest entity.

---
+++ Application/IGuestRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel_Reservation_System.Domain.Entities;
+
+
+namespace Hotel_Reservation_System.Application
+{
+    public interface IGuestRepository
+    {
+        IReadOnlyList<Guest> GetGuests();
+        Guest GetGuestById(int guestId);
+        void AddGuest(Guest guest);
+   
+
+        
+    }
+}
