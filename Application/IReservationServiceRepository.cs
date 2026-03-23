7ac9e3266a6ac24ae8471cf5985120e3ee6cb6d2
Presi <peciceci736@gmail.com>
2026-03-23 09:10:05 -0900

    Add IReservationServiceRepository interface
    Introduced the IReservationServiceRepository interface in the Application namespace to manage ReservationService entities. The interface includes methods for retrieving all services, fetching by ID, and adding new services. Required using directives were also added.

---
+++ Application/IReservationServiceRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel_Reservation_System.Domain.Entities;
+
+namespace Hotel_Reservation_System.Application
+{
+    public interface IReservationServiceRepository
+    {
+        IReadOnlyList<ReservationService> GetReservationServices();
+        ReservationService GetReservationServiceById(int reservationServiceId);
+        void AddReservationService(ReservationService rs);
+    }
+}
