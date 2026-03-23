d5af39ef658356658fece9462cca90c6843d9fd4
Presi <peciceci736@gmail.com>
2026-03-23 09:12:03 -0900

    Add IRoomRepository interface for room data operations
    Introduced the IRoomRepository interface in the Application namespace, defining methods for retrieving all rooms, fetching a room by ID, and adding a new room. Included necessary using directives and referenced the Room entity. This sets up a contract for room-related data access in the system.

---
+++ Application/IRoomRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel_Reservation_System.Domain.Entities;
+
+namespace Hotel_Reservation_System.Application
+{
+    public interface IRoomRepository
+    {
+        IReadOnlyList<Room> GetRooms();
+        Room GetRoomById(int roomId);
+        void AddRoom(Room room);
+    }
+}
