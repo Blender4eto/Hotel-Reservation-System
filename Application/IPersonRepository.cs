c64e626a9e8d6c6cf81c10e42219469ea4836c03
Presi <peciceci736@gmail.com>
2026-03-23 09:03:11 -0900

    Add IPersonRepository interface for person entity management
    Introduced IPersonRepository in the Application namespace with methods to get all people, retrieve a person by ID, and add a new person. Included necessary using directives for collections and the Person entity.

---
+++ Application/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel_Reservation_System.Domain.Entities;
+
+namespace Hotel_Reservation_System.Application
+{
+    public interface IPersonRepository
+    {
+        IReadOnlyList<Person> GetPeople();
+        Person GetPersonById(int personId);
+        void AddPerson(Person person);
+    }
+}
