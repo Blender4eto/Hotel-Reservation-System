8e4367ea8b879c8ed1d1bcc89c9ba31aa2ae6c6a
Presi <peciceci736@gmail.com>
2026-03-23 09:14:59 -0900

    Add IStaffRepository interface for staff management
    Introduced the IStaffRepository interface in the Application namespace to define methods for retrieving all staff, fetching staff by ID, and adding new staff members. Included necessary using directives and referenced the Staff entity from the Domain.Entities namespace. This lays the groundwork for staff-related data operations.

---
+++ Application/IStaffRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel_Reservation_System.Domain.Entities;
+
+namespace Hotel_Reservation_System.Application
+{
+    public interface IStaffRepository
+    {
+        IReadOnlyList<Staff> GetStaff();
+        Staff GetStaffById(int staffId);
+        void AddStaff(Staff staff);
+    }
+}
