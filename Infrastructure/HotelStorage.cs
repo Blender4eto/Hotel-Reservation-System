93e6310d21c7ff0451f537ed10b9abfa275452f4
Presi <peciceci736@gmail.com>
2026-03-24 08:05:04 -0900

    Add HotelStorage class for in-memory entity storage
    Introduced the HotelStorage class in the Infrastructure namespace to manage in-memory collections of Guests, Staff, Rooms, Reservations, ReservationServices, and Persons. Includes a NextId property for ID tracking and initializes all entity lists.

---
+++ Infrastructure/HotelStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hotel_Reservation_System.Domain.Entities;
+
+namespace Hotel_Reservation_System.Infrastructure
+{
+    public class HotelStorage
+    {
+        public int NextId { get; set; } = 1;
+        public List<Guest> Guests { get; set; } = new ();
+         public List<Staff> Staff { get; set; } = new ();
+        public List<Room> Rooms { get; set; } = new ();
+         public List<Reservation> Reservations { get; set; } = new ();
+         public List<ReservationService> ReservationServices { get; set; } = new ();
+         public List<Person> Persons { get; set; } = new ();
+    }
+}
