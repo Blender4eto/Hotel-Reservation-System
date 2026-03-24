ed8f6b90b4f0df879b2e17e4489ba2b96f11969c
Presi <peciceci736@gmail.com>
2026-03-24 08:13:22 -0900

    Add FileStorage class for JSON-based HotelStorage persistence
    Introduced FileStorage in the Infrastructure namespace to handle loading and saving HotelStorage objects using JSON serialization with System.Text.Json. The class provides methods to read from and write to a specified file path, ensuring reliable persistence of hotel data.

---
+++ Infrastructure/FileStorage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Hotel_Reservation_System.Infrastructure
+{
+    public class FileStorage
+    {
+        private readonly string path;
+
+        public FileStorage(string path)
+        {
+            this.path = path;
+        }
+        public HotelStorage Load()
+        {
+            if (!File.Exists(path))
+            {
+                return new HotelStorage();
+            }
+            var json = File.ReadAllText(path);
+
+            var storage = JsonSerializer.Deserialize<HotelStorage>(json);
+            if (storage == null)
+                throw new Exception("Deserialization return null.");
+
+            return storage;
+        }
+        public void Save(HotelStorage storage)
+        {
+            var json = JsonSerializer.Serialize(storage);
+          
+            File.WriteAllText(path, json);
+        }
+    }
+}
