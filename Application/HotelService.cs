307a7008ae8e2120b929a01a1fd4446b4d2a464e
Presi <peciceci736@gmail.com>
2026-03-24 07:40:40 -0900

    Add HotelService class with repository dependencies
    Introduced the HotelService class in the Application layer, including constructor-based dependency injection for all relevant repository interfaces. No business logic implemented yet; this sets up the service structure for future hotel operations.

---
+++ Application/HotelService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel_Reservation_System.Domain.Entities;
+using Hotel_Reservation_System.Domain.Enums;
+//using Hotel_Reservation_System.Domain.ValueObjects;
+
+namespace Hotel_Reservation_System.Application
+{
+    public class HotelService
+    {
+        private readonly IGuestRepository guestRepository;
+        private readonly IPersonRepository personRepository;
+        private readonly IReservationRepository reservationRepository;
+        private readonly IReservationServiceRepository reservationServiceRepository;
+        private readonly IRoomRepository roomRepository;
+        private readonly IStaffRepository staffRepository;
+
+        public HotelService(IGuestRepository guestRepository, IPersonRepository personRepository, IReservationRepository reservationRepository, IReservationServiceRepository reservationServiceRepository, IRoomRepository roomRepository, IStaffRepository staffRepository)
+        {
+            this.guestRepository = guestRepository;
+            this.personRepository = personRepository;
+            this.reservationRepository = reservationRepository;
+            this.reservationServiceRepository = reservationServiceRepository;
+            this.roomRepository = roomRepository;
+            this.staffRepository = staffRepository;
+        }
+
+
+
+    }
+}
