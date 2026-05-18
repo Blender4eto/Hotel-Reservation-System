31d70cda4af0764e32c9a04a4b311a9699bc84a7
presi <peciceci736@gmail.com>
2026-05-18 14:58:45 +0300

    Deleted comment

--- Domain/Entities/Reservation.cs
+++ Domain/Entities/Reservation.cs
@@ -6,47 +6,11 @@ using System.Threading.Tasks;
 
 namespace Hotel_Reservation_System.Domain.Entities
 {
-    //public class Reservation
-    //{
+   
 
-    //    public decimal FinalPrice
-    //    {
-    //        get
-    //        {
-    //            decimal servicesPrice = 0;
 
-    //            foreach (var service in Services)
-    //            {
-    //                servicesPrice += service.PricePerDay * service.DurationInDays;
-    //            }
-
-    //            return (Room.Price * Days) + servicesPrice;
-    //        }
-    //    }
-
-    //    public Reservation(Room room, Guest guest, int days, int id)
-    //    {
-    //        if (days <= 0)
-    //        {
-    //            throw new ArgumentException("Days must be more than 0.");
-    //        }
-
-    //        this.Room = room;
-    //        this.Guest = guest;
-    //        this.Days = days;
-    //        Id = id;
-    //    }
-
-    //    public void AddService(ReservationService service)
-    //    {
-    //        if (service.DurationInDays > Days)
-    //        {
-    //            throw new ArgumentException("Service duration cannot exceed reservation duration.");
-    //        }
-
-    //        Services.Add(service);
-    //    }
-    //}   
+    
+      
     public class Reservation
     {
         public Room Room { get; set; }
@@ -102,4 +66,4 @@ namespace Hotel_Reservation_System.Domain.Entities
             Services.Add(service);
         }
     }
-}
+} 
