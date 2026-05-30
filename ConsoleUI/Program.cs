6ce7c2a62607cc3dd8eaad2885e8b17cca1e1a90
Blender4eto <denka050908@gmail.com>
2026-05-30 19:17:44 +0300

    Updated Program.cs repos to Sql

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Hotel_Reservation_System.Application;
 using Hotel_Reservation_System.Application.Interfaces;
+using Hotel_Reservation_System.Infrastructure;
 using Hotel_Reservation_System.Infrastructure.Json;
+using Hotel_Reservation_System.Infrastructure.Sql;
+using Microsoft.EntityFrameworkCore;
+using static Hotel_Reservation_System.Infrastructure.AppDbContext;
 
 namespace Hotel_Reservation_System.ConsoleUI
 {
@@ -9,13 +13,17 @@ namespace Hotel_Reservation_System.ConsoleUI
         static void Main(string[] args)
         {
             var storage = new FileStorage("hotel.json");
+            //Преси, смени си стринга на твойя, като го ползваш
+            var options = new DbContextOptionsBuilder<AppDbContext>().UseSqlServer("Data Source=BLENDERTOP\\SQLEXPRESS;Database=CodeFirstDb;Integrated Security=True;TrustServerCertificate=True;").EnableSensitiveDataLogging().Options;
+            var db = new AppDbContext(options);
 
-            IGuestRepository guestRepo = new FileGuestRepository(storage);
+            IGuestRepository guestRepo = new SqlGuestRepository(db);
+            IReservationRepository reservationRepo = new SqlReservationRepository(db);
+            IRoomRepository roomRepo = new SqlRoomRepository(db);
+            IStaffRepository staffRepo = new SqlStaffRepository(db);
+            //maybe these are useless repos?
             IPersonRepository personRepo = new FilePersonRepository(storage);
-            IReservationRepository reservationRepo = new FileReservationRepository(storage);
             IReservationServiceRepository reservationServiceRepo = new FileReservationServiceRepository(storage);
-            IRoomRepository roomRepo = new FileRoomRepository(storage);
-            IStaffRepository staffRepo = new FileStaffRepository(storage);
 
             var service = new HotelService(guestRepo, personRepo, reservationRepo, reservationServiceRepo, roomRepo, staffRepo);
             var ui = new HotelUI(service);
