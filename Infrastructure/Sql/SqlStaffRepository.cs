7b4a69cff6eda071e5d33e8e683ee2f93f6bc33e
Blender4eto <denka050908@gmail.com>
2026-05-30 19:17:14 +0300

    Added SqlStaffRepo

---
+++ Infrastructure/Sql/SqlStaffRepository.cs
@@ -0,0 +1,42 @@
+﻿using Hotel_Reservation_System.Application.Interfaces;
+using Hotel_Reservation_System.Domain.Entities;
+using Hotel_Reservation_System.Infrastructure.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Reservation_System.Infrastructure.Sql
+{
+    public class SqlStaffRepository : IStaffRepository
+    {
+        AppDbContext db;
+
+        //Trqbva da go pregledam(ne cheti tova denka)
+        public SqlStaffRepository(AppDbContext db)
+        {
+            this.db = db;
+        }
+        public IReadOnlyList<Staff> GetStaff()
+        {
+            return db.Staffs.ToList();
+        }
+        public Staff GetStaffById(int staffId)
+        {
+            foreach (var staff in db.Staffs)
+            {
+                if (staff.Id == staffId)
+                {
+                    return staff;
+                }
+            }
+            throw new Exception("Staff not found");
+        }
+        public void AddStaff(Staff staff)
+        {
+            db.Staffs.Add(staff);
+            db.SaveChanges();
+        }
+    }
+}
